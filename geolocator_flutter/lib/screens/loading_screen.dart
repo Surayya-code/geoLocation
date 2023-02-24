a0d03e5be5b7c0812128e061539705daa5d45613
Surayya-code <surayyaah@code.edu.az>
2023-02-24 19:21:19 +0400

    geolocation

---
+++ geolocator_flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:geolocator_flutter/services/location.dart';
+ 
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+  
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+  void getLocation() async {
+    Location location=Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: Center(
+      //   child: ElevatedButton(
+      //     onPressed: () {
+      //       //Get the current location
+      //       getLocation();
+      //     },
+      //     child: Text('Get Location'),
+      //   ),
+      // ),
+    );
+  }
+}
